2cd4a4759450a37b737dc1afbdca23dd25f1d5c5
AshikaChadaga <ashikachadaga@gmail.com>
2021-10-03 20:42:53 +0530

    REFACTORED: Formatted

--- src/main/java/com/bridgelabz/addressbooksystemjdbc/AddressBookMain.java
+++ src/main/java/com/bridgelabz/addressbooksystemjdbc/AddressBookMain.java
@@ -9,7 +9,7 @@ public class AddressBookMain {
 	public static void main(String[] args) {
 
 		System.out.println("---------- Welcome To Address Book Program!! ----------");
-		AddressBookDirectory addressBookDirectory = new AddressBookDirectory();
+		AddressBookManager addressBookDirectory = new AddressBookManager();
 		addressBookDirectory.operationDirectory();
 		
 	}
